71e01d48edc4f26c463b31546d18e2f97dc06767
Anant <225331155+Anant33316@users.noreply.github.com>
2026-06-22 07:40:51 +0530

    Time: 48 ms (5.06%), Space: 44.8 MB (91.9%) - LeetHub

---
+++ 3745-maximize-expression-of-three-elements/3745-maximize-expression-of-three-elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximizeExpressionOfThree(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                for(int k=0;k<nums.length;k++){
+                    if(i!=k && i!=j && j!=k){
+                        int sum=nums[i]+nums[j]-nums[k];
+                        max = Math.max(sum,max);
+                    }
+                }
+            }
+        }
+        return max;
+    }
+}
