2b404b926a6cfeded0d9e2523fd1a6e1215e8b41
Anant <225331155+Anant33316@users.noreply.github.com>
2026-09-03 18:45:33 +0530

    Time: 29 ms (15.94%), Space: 118.5 MB (95.65%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,21 +1,10 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
         Arrays.sort(nums1);
-        int[] arr2 = new int[nums1.length];
-        boolean isodd=false;
-        boolean iseven=false;
         for(int i=0;i<1;i++){
-            if(nums1[i]%2==0){
-                iseven=true;
-                arr2[i]=nums1[i];
+            if(nums1[i]%2!=0){
+                return true;
             }
-            else{
-                isodd = true;
-                arr2[i]=nums1[i];
-            }
-        }
-        if(isodd){
-            return true;
         }
         for(int i=0;i<nums1.length;i++){
             if(nums1[i]%2!=0){
