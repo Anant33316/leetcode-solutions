8d5d17e6832b611570bd0e191bbab59b602b294c
Anant <225331155+Anant33316@users.noreply.github.com>
2026-09-08 07:11:30 +0530

    Time: 1 ms (83.65%), Space: 42.4 MB (72.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -3,10 +3,7 @@ class Solution {
         if(n<1000){
             return 0;
         }
-        int count=0;
-        for(int i=1000;i<=n;i++){
-            count++;
-        }
+        int count=n-1000+1;
         return count;
     }
 }
